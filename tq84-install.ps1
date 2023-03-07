75032aacf40f1d4030f207ec134507e367fd0c25
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2023-03-07 23:23:56 +0100

    fix path

--- tq84-install.ps1
+++ tq84-install.ps1
@@ -1,8 +1,8 @@
-rm $env:userprofile/.vscode/extensions/.obsolete
+rm $env:userprofile/.vscode/extensions/extensions.json -ea ignore
+rm $env:userprofile/.vscode/extensions/.obsolete       -ea ignore
 
 $dir  = initialize-emptyDirectory $env:userprofile\.vscode\extensions\vscodevim
 mkdir $dir/out
 
-cp package.json      $dir
-
-cp out/extension.js  $dir/out
+cp package.json                           $dir
+cp vim-1.25.2\extension\out\extension.js  $dir/out
